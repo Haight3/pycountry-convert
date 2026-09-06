c0f79e62385011b754b0ffbe7488f41dca1c65d4
Ismail Baris <ismail.baris@dlr.de>
2026-09-06 17:31:58 +0200

    refactor(resources): relocate data and example resources

--- resources/data/get_country_name_to_alpha2.py
+++ resources/data/get_country_name_to_alpha2.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 #  @namespace pycountry-convert
 
-import get_countries_wikipedia
+import resources.data.get_countries_wikipedia as get_countries_wikipedia
 import pprintpp
 pprintpp.pprint(get_countries_wikipedia.get_alpha3_codes_to_alpha2_codes_from_wiki())
