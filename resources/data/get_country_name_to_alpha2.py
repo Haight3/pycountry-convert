69ac5ab7a9faae2a4fcfd88d86d40823a51148a3
Ismail Baris <ismail.baris@dlr.de>
2026-09-07 09:14:02 +0200

    refactor(get_country_name_to_alpha2.py): import Wikipedia data module explicitly

--- resources/data/get_country_name_to_alpha2.py
+++ resources/data/get_country_name_to_alpha2.py
@@ -2,6 +2,6 @@
 
 import pprintpp
 
-import resources.data.get_countries_wikipedia as get_countries_wikipedia
+from resources.data import get_countries_wikipedia
 
 pprintpp.pprint(get_countries_wikipedia.get_alpha3_codes_to_alpha2_codes_from_wiki())
